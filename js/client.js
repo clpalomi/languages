26cd2a0c0b14786817be8af13e2579fbdcc13556
Claudio Palominos <clpalomi@users.noreply.github.com>
2025-09-20 23:36:39 +0200

    Create client.js

---
+++ js/client.js
@@ -0,0 +1,5 @@
+// Single shared Supabase client
+import { SUPABASE_URL, SUPABASE_ANON_KEY } from './constants.js';
+import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2.45.4/+esm';
+
+export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
