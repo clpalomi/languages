59b0cb93a552cdf94f9164c73ef02d56b0c67ea9
Claudio Palominos <clpalomi@users.noreply.github.com>
2025-09-20 23:36:07 +0200

    Create constants.js

---
+++ js/constants.js
@@ -0,0 +1,3 @@
+// Fill with your Supabase project keys (safe to expose anon key in browser)
+export const SUPABASE_URL = "https://mguftvozgewqtvzqfcfa.supabase.co";
+export const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1ndWZ0dm96Z2V3cXR2enFmY2ZhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTc3ODU5MzQsImV4cCI6MjA3MzM2MTkzNH0.xvYTiXb7M0HoS97mn2LvgBg6LEvCJLUt_jrd0_u41nY";
